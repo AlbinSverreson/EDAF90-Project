3fd8a00fd332435c371bf89cda3cbf23ee1aab59
AlbinSverreson <albin.sverreson@gmail.com>
2021-03-09 15:00:09 +0100

    added "you are logged in"-text

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -3,26 +3,34 @@ import firebase from 'firebase/app';
 import 'firebase/auth'
 import { AngularFireAuth } from "@angular/fire/auth";
 
+
 @Injectable({
   providedIn: 'root'
 })
 
 export class AuthService {
 
+public loggedIn = false;
+
   constructor(
     public afAuth: AngularFireAuth, // Inject Firebase auth service
-  ) { }
+  ) { this.loggedIn = !!sessionStorage.getItem('user')}
 
   // Sign in with Google
   GoogleAuth() {
     return this.AuthLogin(new firebase.auth.GoogleAuthProvider());
+  }
+
+  isLoggedin() {
+    return this.loggedIn;
   }  
 
   // Auth logic to run auth providers
   AuthLogin(provider : firebase.auth.GoogleAuthProvider) {
     return this.afAuth.signInWithPopup(provider)
     .then((result) => {
-        console.log('You have been successfully logged in!')
+        console.log('You have been successfully logged in!');
+        this.loggedIn = true;
     }).catch((error) => {
         console.log(error)
     })
