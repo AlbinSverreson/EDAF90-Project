0f34751ca215e76fff5ac7343fd344d6e10e4604
LeFilipTran <le5116tr-s@student.lu.se>
2021-03-10 18:48:01 +0100

    Add retrieving data from database

--- src/app/quiz-overview/quiz-overview.component.ts
+++ src/app/quiz-overview/quiz-overview.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import firebase from 'firebase/app';
+import { Observable } from 'rxjs';
+import 'firebase/database';
 
 @Component({
   selector: 'app-quiz-overview',
   templateUrl: './quiz-overview.component.html',
-  styleUrls: ['./quiz-overview.component.css']
+  styleUrls: ['./quiz-overview.component.css'],
+  
 })
+
+
+
 export class QuizOverviewComponent implements OnInit {
+  
+  constructor() {
+    
+    const userId = sessionStorage.getItem('user');
+    const db = firebase.database();  
 
-  constructor() { }
-
+    let ref = db.ref('users/' + userId);
+    ref.on('value', readUserQuizes);
+  
+  }
+  
   ngOnInit(): void {
   }
+  
+}
+
+function readUserQuizes(data: any) {
+  console.log(data.val());
 
 }
+
