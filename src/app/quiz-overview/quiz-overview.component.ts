d9234562294f5ce2f1a67f727f5c48cb29599737
LeFilipTran <le5116tr-s@student.lu.se>
2021-03-11 17:27:06 +0100

    Fix not showing quiz name

--- src/app/quiz-overview/quiz-overview.component.ts
+++ src/app/quiz-overview/quiz-overview.component.ts
@@ -12,78 +12,31 @@ import 'firebase/database';
   host: { '[class]': 'quiz' },
 })
 
-
-// const rootQuiz = document.getElementById("show-quiz");
-
 export class QuizOverviewComponent implements OnInit {
   
   @Input() quizes: any;
 
-
-
   constructor(private router: Router, public authService: AuthService) {
     this.quizes = [];
-  }
-  
-  ngOnInit(): void {
     const userId = sessionStorage.getItem('user');
     const db = firebase.database();  
     
     let ref = db.ref('users/' + userId);
     ref.on('value', (data) => {
-
-      const rootQuiz = document.getElementById("show-quiz");
       let user = data.val();
-      let test = { quiz: data.val()};
       let userQuizes = Object.keys(user);
       
-  
-
-      for (let prop in user) {
-        if (user.hasOwnProperty(prop)) {
-           let innerObj = {};
-           innerObj[prop] = user[prop];
-           this.quizes.push(innerObj);
-        }
-     }
-      
-      console.log(   data.val()  );
-      console.log(   userQuizes[0]   );
-      console.log(   this.quizes  );
-      // for(let i = 0; i < this.quizes.length; i++){
-      //   createQuizBox(rootQuiz, data, i);
-      // }
-    });
-    
-    // console.log(ref.on('value', readUserQuizes))
-
-    // const readUserQuizes = (data: any) => {
-      // const rootQuiz = document.getElementById("show-quiz");
-      // let user = data.val();
-      // this.quizes = data.val();
-      // let quizes = Object.keys(user);
-      // console.log(user);
-      // for(let i = 0; i < quizes.length; i++){
-      //   createQuizBox(rootQuiz, data, i);
-      // }
-      // console.log(rootQuiz);
-    // }
-      
-    // function createQuizBox(root, data: any, index) {
-    //   const div = document.createElement("div");
-    //   div.textContent = "Quiz " + index;
-    //   div.classList.add("show-quiz");
-    //   const button = document.createElement("button");
-    //   button.innerHTML = "Take Quiz";
-    //   div.appendChild(button);
-    //   root.appendChild(div);
-
-
-    // }
+      for(let i = 0; i < userQuizes.length; i++) {
+        this.quizes.push(userQuizes[i]);
+      }
+    })
   }
-  takeQuiz() {
-    this.router.navigateByUrl('/do-quiz');
-  };
+  
+  ngOnInit(): void {
+      
+  }
+
+    takeQuiz() {
+      this.router.navigateByUrl('/do-quiz');
+    };
 }
-
-
